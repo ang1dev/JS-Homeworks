e932aeecc8805c52f674a2e3bf38ae273d927fd0
anevceva <anevcheva@gmail.com>
2023-01-30 21:26:14 +0100

    calculate avg od grades

--- student.json/script.js
+++ student.json/script.js
@@ -7,28 +7,33 @@ function getAllStudents() {
                 .then(parsedStudents => {
                     let students = parsedStudents;
 
-                    let averageGrade3 = students
-                        .filter(
-                            (s) => s.averageGrade > 4
-                        );
-                    console.log(averageGrade3);
+                    // let averageGrade3 = students
+                    //     .filter(
+                    //         (s) => s.averageGrade > 4
+                    //     );
+                    // console.log(averageGrade3);
 
-                    let allFemalesWithAvg5 = students
-                        .filter((s) => s.gender === 'Female' && s.averageGrade === 5);
-                    console.log(allFemalesWithAvg5);
+                    // let allFemalesWithAvg5 = students
+                    //     .filter((s) => s.gender === 'Female' && s.averageGrade === 5);
+                    // console.log(allFemalesWithAvg5);
 
-                    let malesStudentsInSkopje18Years = students
-                        .filter((s) => s.gender === 'Male' && s.age > 18 && s.city === 'Skopje');
-                    console.log(malesStudentsInSkopje18Years);
+                    // let malesStudentsInSkopje18Years = students
+                    //     .filter((s) => s.gender === 'Male' && s.age > 18 && s.city === 'Skopje');
+                    // console.log(malesStudentsInSkopje18Years);
+                    
+                    let allFemalesOver24 = students
+                    .filter((s)=> s.gender === 'Female' && s.age > 24)
 
-                    let sumOfAllGrades = students
-                        .map((s) => s.gender === 'Female' && s.age > 24)
-                        .reduce((avg, grade) => (avg += grade), 0);
-                    console.log(sumOfAllGrades);
+                    
+                    let avgOfAllGrades = allFemalesOver24
+                        .map((s)=> s.averageGrade)
+                        .reduce((avg, averageGrade ) => (avg += averageGrade), 0) / allFemalesOver24.length ;
+                    console.log('avg grade is: ',avgOfAllGrades);
 
-                    let studentsWithB = students
-                        .filter((s) => s.firstName.startsWith('B') && s.averageGrade > 2);
-                    console.log(studentsWithB);
+                
+                    // let studentsWithB = students
+                    //     .filter((s) => s.firstName.startsWith('B') && s.averageGrade > 2);
+                    // console.log(studentsWithB);
                 })
                 .catch(e => {
                     console.error(e);
